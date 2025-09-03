66ace07e8f695af9e132889b58bcd4dfb6e4a939
Maksim Kozyrev <masterdigitalit@gmail.com>
2025-09-03 23:00:01 +0300

    name

--- bot/ecosystem.config.js
+++ bot/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "telegram-bot",         // имя процесса
-      script: "./bot.js",           // путь до твоего файла с ботом
+      script: "./index.js",           // путь до твоего файла с ботом
       watch: false,                 // можно включить true, если хочешь автоперезапуск при изменениях
       autorestart: true,            // перезапуск при падении
       max_restarts: 10,             // максимум рестартов подряд
